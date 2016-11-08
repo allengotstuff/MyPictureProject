13e12f6a2141724a5834b3edd76d7bd5b12e2bbf
Allen Sun <410073261@qq.com>
2016-11-09 00:30:13 +0800

    # adding base activity that take care of toolbar and drawer and navigation view

---
+++ app/src/main/java/com/pheth/hasee/stickerhero/BaseActvity.java
@@ -0,0 +1,86 @@
+package com.pheth.hasee.stickerhero;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+
+/**
+ * Created by allengotstuff on 11/8/2016.
+ */
+public class BaseActvity extends AppCompatActivity {
+
+    protected Toolbar mToolbar;
+    protected DrawerLayout mDrawerLayout;
+    protected NavigationView mNavigationView;
+
+
+
+
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        super.setContentView(layoutResID);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.id_nv_menu);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setupToolbar();
+        setupDrawerNav();
+    }
+
+    private void setupDrawerNav(){
+        mNavigationView.setNavigationItemSelectedListener(
+
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+
+                        switch(menuItem.getItemId()){
+                            case R.id.nav_home:
+                                Log.e("Drawer","homeClick");
+                                break;
+                        }
+                        return true;
+                    }
+                });
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDrawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
+            actionBar.setTitle("StickerHero");
+        }
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+}
--- app/src/main/java/com/pheth/hasee/stickerhero/MainActivity.java
+++ app/src/main/java/com/pheth/hasee/stickerhero/MainActivity.java
@@ -2,16 +2,27 @@ package com.pheth.hasee.stickerhero;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
@@ -31,12 +42,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    private RelativeLayout main_container;
-    private Spinner socialSpinner;
-    private ImageView imageView_HeroMode;
-
+public class MainActivity extends BaseActvity {
     public static int OVERLAY_PERMISSION_REQ_CODE_CHATHEAD = 1234;
 
     private TabLayout tabLayout;
@@ -44,162 +50,24 @@ public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initView();
+
     }
 
-    private void initView() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
 
-        socialSpinner = (Spinner) findViewById(R.id.spinner);
+
+    private void initView() {
         tabLayout = (TabLayout) findViewById(R.id.tabLayout);
         myViewPage = (ViewPager) findViewById(R.id.viewpager);
-
-        imageView_HeroMode = (ImageView) findViewById(R.id.iv_hero_mode);
-        imageView_HeroMode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Utils.canDrawOverlays(MainActivity.this)) {
-                    Intent intent = new Intent(MainActivity.this, ChatHeadService.class);
-                    intent.putExtra(Utils.START_HEADCHAT, true);
-                    startService(intent);
-                } else {
-                    requestPermission(OVERLAY_PERMISSION_REQ_CODE_CHATHEAD);
-                }
-
-                FlurryAgent.logEvent(StaticConstant.OPEN_HERO_MODE);
-            }
-        });
-
 
         FragmentPageAdapter myAdapter = new FragmentPageAdapter(getSupportFragmentManager(), this);
         myViewPage.setAdapter(myAdapter);
         myViewPage.setOffscreenPageLimit(2);
         tabLayout.setupWithViewPager(myViewPage);
         tabLayout.setTabMode(TabLayout.MODE_FIXED);
-
-        setupSpinner();
-    }
-
-
-    private void setupSpinner() {
-
-        SpinnerAdapter adapter = new SpinnerAdapter(getBaseContext());
-        socialSpinner.setAdapter(adapter);
-        socialSpinner.setOnItemSelectedListener(adapter);
-
-        //set the default display icon on top of the spinner
-        String[] socialPackage = getResources().getStringArray(R.array.social_package_id);
-        for (int i = 0; i < socialPackage.length; i++) {
-            if (MyApplication.sharedPackage.equals(socialPackage[i])) {
-                socialSpinner.setSelection(i);
-                break;
-            }
-        }
-
-
-    }
-
-    private class SpinnerAdapter extends BaseAdapter implements AdapterView.OnItemSelectedListener {
-
-        private Context mContext;
-        private LayoutInflater inflater;
-        private ArrayList<IconHolder> mList;
-
-        private String[] socialArray = getResources().getStringArray(R.array.social_package_title);
-        private int[] socialIconArray = {R.drawable.whatsapp_icon, R.drawable.messenger_icon};
-
-        private boolean firstTime = true;
-
-        public SpinnerAdapter(Context context) {
-            mContext = context;
-            mList = new ArrayList<>(socialArray.length);
-            for (int i = 0; i < socialArray.length; i++) {
-
-//                if (CommonUtils.isPackageExist(context, socialArray[i])) {
-                IconHolder iconHolder = new IconHolder(socialArray[i], socialIconArray[i]);
-                mList.add(iconHolder);
-//                }
-            }
-            inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-        }
-
-        @Override
-        public int getCount() {
-            return mList.size();
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return mList.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = inflater.inflate(R.layout.single_spinner, parent, false);
-            TextView title = (TextView) convertView.findViewById(R.id.tv_title);
-            ImageView icon = (ImageView) convertView.findViewById(R.id.iv_icon);
-
-            IconHolder iconHolder = mList.get(position);
-            title.setText(iconHolder.getTitle());
-            icon.setImageResource(iconHolder.getIcon());
-            return convertView;
-        }
-
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//            Log.e("onItemSelected",position+"");
-            IconHolder iconHolder = mList.get(position);
-            CommonUtils.saveSocialSetting(mContext, iconHolder.getTitle());
-
-            if (!firstTime) {
-                Toast.makeText(getBaseContext(), "you have swith to " + iconHolder.getTitle() + " shareing", Toast.LENGTH_SHORT).show();
-
-                HashMap<String, String> social_platfor = new HashMap<>();
-                social_platfor.put("Social_Platform", iconHolder.getTitle());
-                FlurryAgent.logEvent(StaticConstant.SELECT_SOCIAL_PLATFORM, social_platfor);
-            }
-            firstTime = false;
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-
-        }
-    }
-
-    private class IconHolder {
-        private String title;
-        private int icon;
-
-        public IconHolder(String title, int icon) {
-            this.title = title;
-            this.icon = icon;
-        }
-
-        public int getIcon() {
-            return icon;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
     }
 
     private void requestPermission(int requestCode) {
